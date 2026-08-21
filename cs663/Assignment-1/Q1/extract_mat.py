771ec9afd11c056d90c1d9cb61cba21bb76b724e
Pushpendra Uikey <pushpendrauikey963@gmail.com>
2026-08-21 23:38:35 +0530

    .mat file extraction since we're working in C++

---
+++ cs663/Assignment-1/Q1/extract_mat.py
@@ -0,0 +1,14 @@
+import scipy.io as sio
+import numpy as np
+
+# Load the MATLAB file
+mat_contents = sio.loadmat('./data/ct.mat')
+
+# The assignment specifies two images: "original" and "subsampled"
+orig_img = mat_contents['original']
+sub_img = mat_contents['subsampled']
+
+# Export them to CSV so C++ can read them
+np.savetxt('temp/ct_original.csv', orig_img, delimiter=',')
+np.savetxt('temp/ct_subsampled.csv', sub_img, delimiter=',')
+print("MAT file extracted successfully!")
