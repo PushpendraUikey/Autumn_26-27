bb907d2d8cb90c45e134961c3ca6334b664c207b
Pushpendra Uikey <pushpendrauikey963@gmail.com>
2026-08-24 16:55:30 +0530

    thread use in class member methods

---
+++ cs787/classwork/24thAug_2026/thread_from_class.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+
+class Event {
+    int val;
+    public:
+    Event() : val(0) {}
+    Event(int v) : val(v) {}
+    void print() {
+        cout << "Event :" << val << endl;
+    }
+};
+
+template <typename T>
+class Cirqueue {
+    T arr[10];
+    int front_index, rear;
+    public:
+        Cirqueue() : front_index(0), rear(0) {}
+        void push(const T& item) {
+            if((rear + 1) % 10 == front_index) {
+                cout << "Queue is full\n";
+                return;
+            }
+            arr[rear] = item;
+            rear = (rear + 1) % 10;
+        }
+        void pop() {
+            if(empty()) {
+                cout << "Queue is empty\n";
+                return;
+            }
+            front_index = (front_index + 1) % 10;
+        }
+        bool empty() const {
+            return front_index == rear;
+        }
+        int size() const {
+            return (rear - front_index + 10) % 10;
+        }
+    
+        T front() {
+            return arr[front_index];
+        }
+};
+
+Cirqueue<Event>* q;
+
+class Producer {
+    int ctr = 0;
+    public:
+    void produce() {
+        while(true) {
+            while(q->size() < 9) {
+                Event e(ctr++);
+                q->push(e);
+            }
+        }
+    }
+};
+
+class Consumer {
+    public:
+    void consume() {
+        while(true) {
+            while(q->empty()) {
+                this_thread::sleep_for(chrono::milliseconds(100));
+            }
+            Event e = q->front();
+            q->pop();
+            e.print();
+        }
+    }
+};
+
+class A {
+    int val;
+    public:
+    A(int v) : val(v) {}
+    void f() {
+        cout << (val++)%10 << endl;
+    }
+};
+
+class B {
+    public:
+    void g() {
+        cout << "world\n";
+    }
+};
+
+int main( int argc, char* argv[]) {
+    // A* a1 = new A(1000);
+    // B* b1 = new B();
+
+    // thread t1(&A::f, a1);
+    // thread t2(&B::g, b1);
+
+    // t1.join();
+    // t2.join();
+
+    q = new Cirqueue<Event>();
+
+    Producer *p = new Producer();
+    Consumer *c = new Consumer();
+
+    thread t3(&Producer::produce, p);
+    thread t4(&Consumer::consume, c);
+
+    t3.join();
+    t4.join();
+}
