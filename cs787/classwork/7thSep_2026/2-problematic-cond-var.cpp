240084344ec909edad945294e87a7c195176075d
Pushpendra Uikey <pushpendrauikey963@gmail.com>
2026-09-07 23:28:04 +0530

    cs787: problematic conditional variable original question

---
+++ cs787/classwork/7thSep_2026/2-problematic-cond-var.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+using namespace std;
+
+
+mutex m;
+condition_variable cv;
+
+void f () {
+unique_lock<mutex> ul(m);
+	while (1) {
+		cv.wait(ul);
+		for (int i=0; i< 4; i++) {
+			cout << this_thread::get_id() << " " << i << endl;
+			this_thread::sleep_for(300ms);
+		}
+		cv.notify_one();
+	}
+};
+
+
+int main () {
+
+ thread t1(f);
+ thread t2(f);
+ this_thread::sleep_for(100ms);
+ cv.notify_one();
+ t1.join();
+ t2.join();
+
+}
+
+
