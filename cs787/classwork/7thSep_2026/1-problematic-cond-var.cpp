b5e6422c31704b08d573dad93472f0fb496ec054
Pushpendra Uikey <pushpendrauikey963@gmail.com>
2026-09-07 23:26:57 +0530

    cs787: one problematic cond var -> a little dangled solution

--- cs787/classwork/7thSep_2026/1-problematic-cond-var.cpp
+++ cs787/classwork/7thSep_2026/1-problematic-cond-var.cpp
@@ -3,21 +3,32 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
-#include <atomic>
 using namespace std;
 
 
 mutex m;
 condition_variable cv;
-atomic<bool> ready{true};
+bool ready = true;
+
 void f () {
 unique_lock<mutex> ul(m);
 	while (1) {
-		cv.wait(ul);
+		if(!ready) {
+			ready = true;
+			cv.wait(ul);
+/*
+This program correctly runs however with one underlying subtle problem that is cv.notify_one()
+is called at the end is before unlocking the lock so it dosn't wake up the other thread from the sleep state
+to run rather since the lock is still held by the current thread it'll be just pushed to mutex queue.
+
+kind of inefficiency to waking from cv queue just to be kept in teh mutex queue.
+*/
+		}
+		ready = false;
 		for (int i=0; i< 4; i++) {
 			cout << this_thread::get_id() << " " << i << endl;
 			this_thread::sleep_for(300ms);
-		}
+		}	
 		cv.notify_one();
 	}
 };
@@ -34,4 +45,8 @@ int main () {
 
 }
 
+/*
+This might not always work if the main thread still somehow executes first and cv.notify_one() is called
+before the other threads are waiting on the condition variable.
+*/
 
